696327b4379f80a695da444ef07b3f81300dbbaf
Marek Wyszyński <45630165+gitgeoman@users.noreply.github.com>
2023-08-01 15:53:44 +0200

    add comments for script.sh

--- scratchpad/script.sh
+++ scratchpad/script.sh
@@ -4,9 +4,9 @@ for i in $(cat url.txt);
 do
 wget -P ./path "${i}"
 echo "${i}" | tee -a sc_res.txt
-zip -FFv ./path/"${i}" --out "${i}"fixed.zip
+# zip -FFv ./path/"${i}" --out "${i}"fixed.zip # naprawa archiwum gdy wali błąd pliku (przepakowanie)
 done
 
 cd path
 unzip \*.zip
-# find . -name "*.zip" -exec rm -rf {} \;
+# find . -name "*.zip" -exec rm -rf {} \; # usuwanie zip
